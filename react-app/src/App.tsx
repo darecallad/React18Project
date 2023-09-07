8a2400e5b7d82fd2d68345fdc34a4d9d18f18f26
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-07 13:54:38 -0700

    update alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
+  const [alert, setAlert] = useState(false);
   return (
     <>
-      <Button color="primary" onClick={() => console.log("click")}>
-        Hello
+      {alert && <Alert onClose={() => setAlert(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={() => setAlert(true)}>
+        Click
       </Button>
     </>
   );
