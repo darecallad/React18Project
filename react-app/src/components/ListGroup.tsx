0ee03b3d1a3af95bc1aac990f6d0be047e8622c8
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-07 12:24:36 -0700

    update ListGRoup

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Franciso", "Los Angles", "London", "Tokyo"];
 
+  //Event Handle
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
