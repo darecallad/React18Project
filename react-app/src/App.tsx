5155bc810d26c89e06b535733b87c6c0ae39bf65
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-07 16:32:00 -0700

    define cart component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,13 @@
-import Like from "./components/Like";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
   return (
     <>
-      <Like onClick={() => console.log("click")} />
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </>
   );
 }
